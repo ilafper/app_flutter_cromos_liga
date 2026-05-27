a9572f6a7cda36016b3cf33b7873ff925012be31
ilafper <ivanaalfaya@gmail.com>
2026-05-27 17:52:13 +0200

    apertura de sobres hecha

--- lib/apiService/apiConect.dart
+++ lib/apiService/apiConect.dart
@@ -2,11 +2,11 @@ import 'dart:convert';
 import 'dart:ffi';
 import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
-
+import '../home.dart';
 class ApiConect{
 
   static const login="http://100.97.249.120:3000/api/login";
-
+  static const sobre = "http://100.97.249.120:3000/api/cromosRamdom";
   static Future<Map<String, dynamic>?> loginApp({
     required String correo,
     required String password,
@@ -17,11 +17,12 @@ class ApiConect{
       headers: {"Content-Type": "application/json"},
 
       body: jsonEncode({
-        
         "correo": correo,
         "password":password
       }),
     );
+
+
 
     //print(response.body);
 
@@ -31,7 +32,26 @@ class ApiConect{
       return res;
     }
   }
-  
 
+  //abrir sobres
+
+  static Future<List<dynamic>> abrirSobres() async {
+    print("LLLAMANDO LLAMANDO PROBANDO PROBAND");
+    // peticion a la api para cargar random sobres
+    final res = await http.get(Uri.parse(sobre));
+    print(res);
+    // si fue exitosa manda la lista
+    if (res.statusCode == 200) {
+      final json = jsonDecode(res.body);
+      // guardamos la lista
+      List data = json["sobre_cartas"];
+
+      print(" uuuuuuuuuuuuuuuu   $data");
+      //devolvemos al lista
+      return data;
+    } else {
+      throw Exception("Error al cargar cromos");
+    }
+  }
 
 }
--- lib/home.dart
+++ lib/home.dart
@@ -1,27 +1,144 @@
 import 'package:flutter/material.dart';
 
-class Home extends StatelessWidget {
+import 'apiService/apiConect.dart';
+
+// en el caso de trabajar con lista dinamcas se tiene que usar  StatefulWidgeten vez de
+class Home extends StatefulWidget {
   const Home({super.key});
-   
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  List sobres = [];
+  int index = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
       appBar: AppBar(
-        title: const Text("Home", style: TextStyle(
-          color: Color.fromARGB(255, 8, 8, 8)
-        ),),
-        
+        title: Text("Home"),
       ),
       body: Center(
         child: Column(
           
           children: [
-            Image.asset("assets/images/sobre.png"),
-            ElevatedButton(onPressed: (){} , child: const Text("ABRIR"))
+            const SizedBox(height: 50),
+            //si no hay cromos
+            if (sobres.isNotEmpty)
+              Stack(
+                alignment: Alignment.center,
+
+                children: [
+                  // CARTAS DEBAJO (efecto pila)
+                  for (int i = index + 1; i < sobres.length; i++)
+                    
+                    Positioned(
+                      bottom: 10.0 * (i - index),
+                      child: Opacity(opacity: 0.5, child: _card(sobres[i])),
+                    ),
+
+                 
+                  GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        if (index < sobres.length - 1) {
+                          index++;
+                        }
+                      });
+                    },
+                    child: _card(sobres[index]),
+                  ),
+                ],
+              ),
+
+            const SizedBox(height: 20),
             
+            if (sobres.isEmpty)
+              Column(
+                children: [
+
+                  Image.asset("assets/images/sobre.png"),
+                  const SizedBox(height: 20),
+                  ElevatedButton(
+                    onPressed: () async {
+                      final data = await ApiConect.abrirSobres();
+
+                      setState(() {
+                        sobres = data;
+                        index = 0;
+                      });
+                    },
+                    child: const Text("ABRIR x1"),
+                  ),
+                ],
+              )
+              
+              
+              else if(sobres.isNotEmpty)
+              Row(
+                children: [
+                    // boton salir
+
+                    ElevatedButton(
+                  onPressed: () async {
+                    
+
+                    setState(() {
+                       setState(() {
+                        sobres = [];
+                        index = 0;
+                      });
+                    });
+                  },
+                  child: const Text("Salir"),
+                  ),
+
+
+                  ElevatedButton(
+                  onPressed: () async {
+                    final data = await ApiConect.abrirSobres();
+
+                    setState(() {
+                      sobres = data;
+                      index = 0;
+                    });
+                  },
+                  child: const Text("Volver a abrir"),
+                  )
+                ],
+              )
+            
+
           ],
         ),
+      ),
+    );
+  }
+
+
+
+
+
+
+  Widget _card(dynamic cromo) {
+    return Container(
+      width: 200,
+      height: 300,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(12),
+        boxShadow: [BoxShadow(blurRadius: 10, color: Colors.black26)],
+      ),
+
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(cromo["nombre"], style: const TextStyle(fontSize: 18)),
+
+          Text(cromo["equipo"]),
+        ],
       ),
     );
   }
